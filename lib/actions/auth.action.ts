df592b94c7cd547c330c9ed5f9e2a09e12333296
Aditya Sinha <iamaditya.sinha16@gmail.com>
2025-05-05 19:32:01 +0530

    Signup signin implemented

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,134 @@
+'use server'
+
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7
+
+export async function signUp(params: SignUpParams) {
+    const {uid, name, email} = params;
+
+    try {
+        const userRecord = await db.collection('users').doc(uid).get()
+
+        if(userRecord.exists) {
+            return {
+                success: false,
+                message: 'User already exists'
+            }
+        }
+
+        await db.collection('users').doc(uid).set({
+            name,
+            email
+        })
+
+        return {
+            success: true,
+            message: 'User created successfully'
+        }
+
+    } catch (error: any) {
+        console.error('Error in signUp:', error);
+
+        if(error.code === 'auth/email-already-exists') {
+            return{
+                success: false,
+                message: 'Email already exists'
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Error in signUp'
+        }
+    }
+}
+
+export async function setSessionCookie(idToken: string){
+    const cookieStore = await cookies()
+
+    const sessionCookie = await auth.createSessionCookie(idToken, {
+        expiresIn: ONE_WEEK * 1000,// 5 days
+    })
+
+    cookieStore.set('session', sessionCookie, {
+        maxAge: ONE_WEEK, // 5 days
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        path: '/',
+        sameSite: 'lax',
+    })
+}
+
+export async function signIn(params: SignInParams) {
+    const {email, idToken } = params
+
+    try {
+        const userRecord = await auth.getUserByEmail(email)
+
+        if(!userRecord) {
+            return {
+                success: false,
+                message: 'User not found'
+            }
+        }
+
+        await setSessionCookie(idToken)
+
+        return {
+            success: true,
+            message: 'Signed in successfully'
+        }
+    } catch (error: any) {
+        console.error('Error in signIn:', error);
+
+        if(error.code === 'auth/user-not-found') {
+            return{
+                success: false,
+                message: 'User not found'
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Error in signIn'
+        }
+    }
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+    const cookieStore = await cookies()
+    const sessionCookie = cookieStore.get('session')?.value
+
+    if (!sessionCookie) {
+        return null
+    }
+
+    try {
+        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true)
+
+        const userRecord = await db.
+            collection('users')
+            .doc(decodedClaims.uid)
+            .get()
+
+        if(!userRecord.exists) {
+            return null
+        }
+
+        return {
+            ...userRecord.data(),
+            id: userRecord.id,
+        } as User
+    } catch (error) {
+        console.log('Error in getCurrentUser:', error);
+        return null
+    }
+}
+
+export async function isAuthenticated() {
+    const user = await getCurrentUser()
+
+    return !!user
+}
